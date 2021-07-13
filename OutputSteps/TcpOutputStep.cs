ac4e868738337663f66c7d227cc9a26dc052a317
dmisav <savchenko.di@gmail.com>
2021-07-13 16:21:04 +0300

    Added TCPSender. Adjusted TCPOutput step

--- OutputSteps/TcpOutputStep.cs
+++ OutputSteps/TcpOutputStep.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -10,7 +9,16 @@ namespace Pipeline.OutputSteps
     public class TcpOutputStepIOutput : IOutput<string>
     {
         private readonly int _port;
+        private readonly string _server;
         private ChannelReader<string> _in;
+        private TcpSender _tcpSender;
+
+
+        public TcpOutputStepIOutput(string server, int port)
+        {
+            _server = server;
+            _port = port;
+        }
 
         public async IAsyncEnumerable<string> ReadFromChannelAsync(CancellationToken ct)
         {
@@ -22,7 +30,17 @@ namespace Pipeline.OutputSteps
 
         public Task StartRoutine(CancellationToken ct)
         {
-            throw new NotImplementedException();
+            return new(async () =>
+            {
+                _tcpSender = new TcpSender();
+                _tcpSender.StartClient(_server, _port);
+                var iterator = _in.ReadAllAsync(ct);
+
+                await foreach (var msg in ReadFromChannelAsync(ct))
+                {
+                    _tcpSender.Send(msg);
+                }
+            }, ct);
         }
 
         public void AssignInputChannel(ChannelReader<string> channel)
