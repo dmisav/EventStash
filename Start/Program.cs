c052a823457b963cb143af33026975cb9b0011a6
dmisav <savchenko.di@gmail.com>
2021-07-13 17:57:23 +0300

    - set solution structure to be regular way. 1 sln + 2 projects  folders in same root folder
    - added DI container to Program start

---
+++ Start/Program.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Start
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+        static void Main(string[] args)
+        {
+            RegisterServices();
+
+            Console.WriteLine("Starting Execution");
+            Console.ReadLine();
+        }
+
+        private static void RegisterServices()
+        {
+            var collection = new ServiceCollection();
+            var builder = new ContainerBuilder();
+            //example of registering type. Add yours
+            //builder.RegisterType<DemoService>().As<IDemoService>;
+
+            builder.Populate(collection);
+            var appContainer = builder.Build();
+            _serviceProvider = new AutofacServiceProvider(appContainer);
+
+            //non autoFac usage
+            //_serviceProvider = collection.BuildServiceProvider();
+            //var service = serviceProvider.GetService<Idemoservice>
+            //service.DoSomewthing()
+        }
+
+        private static void DisposeServices()
+        {
+            if (_serviceProvider == null)
+            {
+                return;
+            }
+            if (_serviceProvider is IDisposable)
+            {
+                ((IDisposable)_serviceProvider).Dispose();
+            }
+        }
+
+    }
+}
