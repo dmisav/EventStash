906adabc19aba8bb6dfdf28cec12be618b145be2
VARONIS\vdobrotskyi <vdobrotskyi@varonis.com>
2021-07-14 14:22:16 +0300

    Round robin tcp sender

---
+++ Pipeline/OutputSteps/RoundRobinTcpOutputStep.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Pipeline.Models;
+
+namespace Pipeline.OutputSteps
+{
+    public class RoundRobinTcpOutputStep : IOutput<string>
+    {
+        private readonly RoundRobinTcpSender _tcpSender;
+        private ChannelReader<string> _in;
+
+        public RoundRobinTcpOutputStep(int port, int socketNumber = 20)
+        {
+            _tcpSender = new RoundRobinTcpSender(port, socketNumber);
+        }
+
+        public Task StartRoutine(CancellationToken ct)
+        {
+            return new(async () =>
+            {
+                while (true)
+                {
+                    await foreach (var msg in ReadFromChannelAsync(ct))
+                    {
+                        _ = _tcpSender.Send(msg);
+                    }
+                }
+            }, ct);
+        }
+
+        public IAsyncEnumerable<string> ReadFromChannelAsync(CancellationToken ct)
+        {
+            return _in.ReadAllAsync(ct);
+        }
+
+        public void AssignInputChannel(ChannelReader<string> channel)
+        {
+            _in = channel;
+        }
+    }
+}
---
+++ Pipeline/OutputSteps/RoundRobinTcpSender.cs
@@ -0,0 +1,102 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pipeline.OutputSteps
+{
+    public class RoundRobinTcpSender
+    {
+        private readonly SocketWrapper[] _sockets;
+        private readonly RoundRobinNumber _roundRobinNumber;
+
+        public RoundRobinTcpSender(int port, int socketNumber)
+        {
+            _sockets = new SocketWrapper[socketNumber];
+            _roundRobinNumber = new RoundRobinNumber(socketNumber);
+
+            for (var i = 0; i < socketNumber; i++)
+            {
+                _sockets[i] = new SocketWrapper(port);
+            }
+        }
+
+        public Task Send(string message)
+        {
+            return Task.Run(() =>
+            {
+                int nextNumber = _roundRobinNumber.GetNextNumber();
+                _sockets[nextNumber].Send(message);
+            });
+        }
+    }
+
+    public class SocketWrapper
+    {
+        private readonly Socket _socket;
+        private readonly int _port;
+
+        public SocketWrapper(int port)
+        {
+            _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            _port = port;
+            Start();
+        }
+
+        public void Start()
+        {
+            try
+            {
+                _socket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+            }
+            catch (SocketException exception)
+            {
+            }
+        }
+
+        public void Send(string message)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(message);
+            SendBytes(msg, true);
+        }
+
+        private void SendBytes(byte[] bytes, bool tryReconnect)
+        {
+            try
+            {
+                _socket.Send(bytes);
+            }
+            catch (SocketException ex)
+            {
+                if (tryReconnect)
+                {
+                    Start();
+                    SendBytes(bytes, false);
+                }
+
+                // drop message
+            }
+        }
+    }
+
+    public class RoundRobinNumber
+    {
+        private readonly int _maxNumbers;
+        private int _lastNumber = 0;
+
+        public RoundRobinNumber(int maxNumbers)
+        {
+            _maxNumbers = maxNumbers;
+        }
+
+        public int GetNextNumber()
+        {
+            int nextNumber = Interlocked.Increment(ref _lastNumber);
+
+            int result = nextNumber % _maxNumbers;
+
+            return result >= 0 ? result : -result;
+        }
+    }
+}
